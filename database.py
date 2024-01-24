5cea349c4984fc108385d929fa3042c7144e0a35
frodrigu3z <149067773+frodrigu3z@users.noreply.github.com>
2024-01-24 21:56:26 +0100

    Update database.py

--- database.py
+++ database.py
@@ -6,10 +6,7 @@ from marshmallow import fields, Schema
 import os
 import base64
 
-# Lee la información existente en un archivo denominado .env
-# Este es un archivo oculto en un servidor de producciómn y alberga
-# las etiquetas y sus valores para la cadena de conexión a MongoDB
-
+# Cargamos las variables de entorno desde el archivo .env
 load_dotenv(find_dotenv())
 usuario = os.environ.get("MYSQLDB_USUARIO")
 password = os.environ.get("MYSQLDB_PASSWORD")
@@ -17,13 +14,17 @@ host = os.environ.get("MYSQLDB_HOST")
 bd = os.environ.get("MYSQLDB_BD")
 
 app = Flask(__name__)
+
+# Configuración de la URI de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{usuario}:{password}@{host}/{bd}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'hardsecretkey'  # Para las sesiones flash
+app.config['SECRET_KEY'] = 'hardsecretkey'
 
+# Inicialización de SQLAlchemy y Marshmallow con la aplicación Flask
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Definición del esquema de Marshmallow para la clase Gorra
 class GorraSchema(Schema):
     id = fields.Int()
     descripcion = fields.Str()
@@ -32,6 +33,7 @@ class GorraSchema(Schema):
     nombre_imagen = fields.Str()
     imagen = fields.Method('get_imagen_base64')
 
+    # método para convertir la imagen a base64
     def get_imagen_base64(self, obj):
         if obj.imagen is not None:
             return base64.b64encode(obj.imagen).decode('utf-8')
